5c8831af7b315921fca9d786b8689cdc702f6eaa
brainburster <zengliyuan0417@gmail.com>
2025-04-04 01:55:33 +0800

    Refactor
    Updated `logger.hpp` with renamed member variables for clarity and enhanced logging methods for better readability.
    Modified `PasswordManagerGUI.hpp` to implement progress tracking for operations like creating and changing password instances. Introduced `SetProgressTarget` for clearer code and enhanced asynchronous task handling with source location information for easier issue tracing.
    
    Refactor project structure and update file paths
    
    Updated `PasswordManagerGUI.vcxproj` to reflect new directory structure, moving source and header files to `src/core`, `src/utility`, and `src/ui`. Adjusted include directives in `application_functional.hpp` and `main.cpp` to point to the new locations. Replaced `imgui_rawstring.txt` with `imgui_layout.inl` in the `main` function.
    
    Refactor logger for improved performance and structure
    
    - Updated `SourceInfo` to use `std::string_view` for efficiency.
    - Introduced `LogTask` structure for better log management.
    - Added abstract `LogSink` class with `DefaultConsoleSink` and `DefaultFileSink` implementations.
    - Enhanced `Logger` class with a background thread and updated member variables.
    - Renamed logging mask to `default_mask_` for clarity.
    - Modified logging methods to utilize `default_mask_`.
    - Improved `ParseSourceInfo` and regex handling with `std::string_view`.
    - Introduced new constants for buffer sizes to enhance logging performance.
    
    Refactor Logger class and enhance application structure
    
    Significantly refactored the `Logger` class to improve functionality and structure. Introduced a new `LogSink` interface for flexible logging outputs, added asynchronous logging support, and enhanced logging format with detailed source information. Updated initialization and cleanup processes with `APP_Init` `APP_Loop` and `APP_Cleanup` functions. Improved the password manager GUI layout and functionality for better user experience.

---
+++ Project/build/src/main.cpp
@@ -0,0 +1,88 @@
+
+#include <stdio.h>
+#include "ui/PasswordManagerGUI.hpp"
+#include <csignal>
+
+// Main code
+int main(int, char**)
+{
+	Logger::Instance().Init();
+	Logger::Instance().Debug().Log("hello {}", "world!");
+
+	{
+		auto log_stream = Logger::Instance().Info().Stream();
+		log_stream << 4 << 2;
+	}
+
+	std::signal
+	(
+		SIGABRT, 
+		[](int sig)
+		{
+			Logger::Instance().Fatal().Log
+			(
+				"event = [signal_abort], signal = [{}], description = [SIGABRT triggered, application will now exit.]",
+				sig
+			);
+			APP_Cleanup(CurrentApplicationData);
+			std::_Exit(1);
+		}
+	);
+
+	std::set_terminate
+	(
+		[]()
+		{
+			if (auto ex = std::current_exception())
+			{
+				try 
+				{
+					std::rethrow_exception(ex);
+				}
+				catch (const std::exception& e) 
+				{
+					Logger::Instance().Fatal().Log
+					(
+						"event = [uncaught_exception], type = [std::exception], message = [{}]", e.what()
+					);
+				}
+				catch (...)
+				{
+					Logger::Instance().Fatal().Log("event = [uncaught_exception], type = [unknown]");
+				}
+			}
+			else
+			{
+				Logger::Instance().Fatal().Log("event = [terminate], reason = [unknown]");
+			}
+			APP_Cleanup(CurrentApplicationData);
+			std::abort();
+		}
+	);
+
+	try
+	{
+		auto SG = MakeScopeGuard
+		(
+			[](ApplicationData&) 
+			{
+			APP_Cleanup(CurrentApplicationData);
+			}, 
+			std::ref(CurrentApplicationData)
+		);
+
+		APP_Init(CurrentApplicationData);
+
+		APP_Loop(CurrentApplicationData);
+	}
+	catch (std::exception& e)
+	{
+		Logger::Instance().Fatal().Log
+		(
+			"event = [exception_caught], type = [std::exception], message = [{}]", e.what()
+		);
+		throw;
+	}
+
+	return 0;
+}
